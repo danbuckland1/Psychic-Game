f6069038571d061d6c6cfb9dcc8d4c7726b2cefb
danbuckland1 <buckd409@gmail.com>
2018-06-06 19:46:22 -0400

    Working on coding from my pseudo code.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
 
-
     var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     var wins = 0;
     var losses = 0;
@@ -10,27 +9,38 @@
     document.onkeyup=function(){
         var userguess = String.fromCharCode(event.keyCode).
         toLowerCase()
-        console.log(letters.length);
-    
-         var computerguess = letters[Math.floor(Math.random()* letters.length)];
 
+        console.log(userguess);
         
+        var computerguess = letters[Math.floor(Math.random()* letters.length)];
 
+        console.log(computerguess);
+
+         // If userguess is the same as (type and value) computer guess is a wins
+         // If win, count goes up
+         if ((userguess === computerguess)){
+             wins++;
+         }
         
-
-
+        // If userguess is not the same as computerguess, Guess Left decriment down one
+        if((userguess !== computerguess)){
+            guessesLeft--;
+        } 
+        // If decriment counter is 0 it's a loss count goes up
+        
 
 
 
         var html = "<p>Guess what letter I'm thinking of!</p>" +
-            "<p>Wins: " + wins + "</p>" +
-            "<p>Losses: " + losses + "</p>" +
-            "<p>Guess Left: " + guessesLeft + "</p>" +
-            "<p> Your Guesses So Far: " + guessSoFar + "</p>";
-
-            document.querySelector('#game').innerHTML = html;
-
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + guessesLeft + "</p>" +
+        "<p>Guesses So Far: " + guessSoFar + "</p>";
+    
+        document.querySelector('#game').innerHTML = html;
     }
+
+    
 
     
     
