6431dde60d437aa796069f1e92c7323e3b37566a
danbuckland1 <buckd409@gmail.com>
2018-07-23 16:00:13 -0400

    Fixing variables and adding the game start function as a reset.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,13 @@
     var wins = 0;
     var losses = 0;
     var guessesLeft = 10;
-    var guessSoFar = [];
-    
+    var guessesSoFar = [];
+
+    function gameStart() {
+        guessesLeft = 9;
+        guessesSoFar = [];
+        console.log("Game Start")
+    }
 
     document.onkeyup=function(){
         var userguess = String.fromCharCode(event.keyCode).
@@ -16,34 +21,40 @@
 
         console.log(computerguess);
 
+        guessesSoFar.push(userguess);
+
+        console.log(guessesSoFar);
+
          // If userguess is the same as (type and value) computer guess is a wins
          // If win, count goes up
-         if ((userguess === computerguess)){
+         if ((userguess == computerguess)){
              wins++;
+
+             guessesLeft = 9;
+             guessesSoFar = 0;
+            gameStart();
          }
         
         // If userguess is not the same as computerguess, Guess Left decriment down one
-        if((userguess !== computerguess)){
-            guessesLeft--;
+        else if(guessesLeft == 0){
+            losses ++;
+
+            guessesSoFar = 0;
+            guessesLeft = 9;
+
+            gameStart();
         } 
         // If decrement counter is 0 it's a loss count goes up
-        if((guessesLeft <= 0)){
-            losses ++; 
+        else if(userguess !== computerguess){
+
+            guessesLeft--;
         }
-
-        // Reset decrement counter
-       
-       
-        // Display guesses so far
-        
-
-
 
         var html = "<p>Guess what letter I'm thinking of!</p>" +
         "<p>Wins: " + wins + "</p>" +
         "<p>Losses: " + losses + "</p>" +
         "<p>Guesses Left: " + guessesLeft + "</p>" +
-        "<p>Guesses So Far: " + guessSoFar + "</p>";
+        "<p>Guesses So Far: " + guessesSoFar + "</p>";
     
         document.querySelector('#game').innerHTML = html;
     }
